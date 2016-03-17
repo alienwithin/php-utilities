da933703b416ae090813c1877ee62145bd550b9d
Munir Njiru <alienwithin@users.noreply.github.com>
2016-03-17 16:47:15 +0300

    Update md5-dictionary-crack.php

--- md5-dictionary-crack.php
+++ md5-dictionary-crack.php
@@ -37,7 +37,7 @@ Current Hash: b8cd8521e1039235c37db676a1a29d2a
 Current Hash: 2054aa40547bcfb7b8eb9ee1f10055d5
 
 String Matching Hash Found:
- zythem
+zythem
 
 Hash Cracked in 2.0806159973145 seconds.
 
@@ -47,30 +47,28 @@ error_reporting(0);
 
 //Start Timer to Get how long it takes to run it.
 
-$start = microtime(true); 
- 
-$lines = file($_SERVER["argv"][1]);
-$hash_to_crack=$_SERVER["argv"][2];
+$start = microtime(true);
+
+$lines         = file($_SERVER["argv"][1]);
+$hash_to_crack = $_SERVER["argv"][2];
 
 
-if ($lines==""){
+if ($lines == "") {
     echo "No Dictionary Specified \n";
     echo "Usage: php crack.php dictionary_file hash_to_test_for_match e.g. \n \nphp crack.php out.txt 25e4ee4e9229397b6b17776bfceaf8e7";
-}
-else{
-    foreach($lines as $line)
-    {
-      $encr=md5($line);
-      echo ("Current Hash: ". $encr."\n");
-      
-	if ($encr==$hash_to_crack){
-	echo("\nString Matching Hash Found:\n ".$line."\n");
-
-
-	$time_elapsed_us = microtime(true) - $start;	
-	echo "Hash Cracked in $time_elapsed_us seconds.\n\n";
-	exit;
-	}
+} else {
+    foreach ($lines as $line) {
+        $encr = md5($line);
+        echo ("Current Hash: " . $encr . "\n");
+        
+        if ($encr == $hash_to_crack) {
+            echo ("\nString Matching Hash Found:\n " . $line . "\n");
+            
+            
+            $time_elapsed_us = microtime(true) - $start;
+            echo "Hash Cracked in $time_elapsed_us seconds.\n\n";
+            exit;
+        }
     }
 }
 
